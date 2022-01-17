621c9a6d26d7e5db5865c234069196f106db1988
odrling <florianbadie@odrling.xyz>
2022-01-17 14:16:34 +0100

    producer to show warnings

--- kafka_producer.py
+++ kafka_producer.py
@@ -9,18 +9,17 @@ from commands import Command, ElementEvent, SetXMICommand
 async def send():
     producer = AIOKafkaProducer(bootstrap_servers='localhost:9092', acks='all')
     # Produce message
-    # create an instance of User v1
     with open("/home/odrling/eclipse-workspaces/gemoc-xbpmn/test.bpmn/examples/process_1.bpmn") as f:  # noqa
-        user = Command(command=SetXMICommand(f.read()))
+        process = Command(command=SetXMICommand(f.read()))
 
-    event = Command(command=ElementEvent(elementID="Task_1", event="Start"))
+    event = Command(command=ElementEvent(elementID="Task_2", event="Start"))
 
     topic_send = partial(producer.send_and_wait, "my_topic10")
 
     try:
         await producer.start()
-        # create the message
-        await topic_send(user.serialize())
+        # create the messages
+        await topic_send(process.serialize())
         await topic_send(event.serialize())
 
         print("done")
